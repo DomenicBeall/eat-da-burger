ff32ee62f28f0c1fecf23ed2c53bff9f9238d1f8
Domenic Beall <domenicbeall2@gmail.com>
2021-02-22 17:30:13 +1100

    Added seed to test eaten burger

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,5 @@ USE burgers_db;
 INSERT INTO burgers (burger_name) VALUES ("MCDouble");
 INSERT INTO burgers (burger_name) VALUES ("Big Mac");
 INSERT INTO burgers (burger_name) VALUES ("Double Double burger");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Eaten Burger", 1);
