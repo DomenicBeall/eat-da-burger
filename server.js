347d9135dce6a55d0c744655b0824f0125bb741e
Domenic Beall <domenicbeall2@gmail.com>
2021-02-22 17:30:36 +1100

    Created express server and added routing

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-const express = require("express");
+const e = require('express');
+const express = require('express')
+const exphbs = require('express-handlebars');
+const burger = require('./models/burger.js');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    var eatenBurgers = [];
+    var uneatenBurgers = [];
+
+    burger.getAll((results) => {
+        results.forEach(element => {
+            if (element.devoured === 0)
+                uneatenBurgers.push(element);
+            else
+                eatenBurgers.push(element);
+        });
+
+        res.render("index", { eatenBurgers, uneatenBurgers });
+    });    
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at ${port}`)
+});
