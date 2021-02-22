e25efd75f8b72f9a01109262ff83d74ca610c551
Domenic Beall <domenicbeall2@gmail.com>
2021-02-22 17:32:14 +1100

    Created orm methods

--- config/orm.js
+++ config/orm.js
@@ -2,21 +2,21 @@ const connection = require("./connection.js");
 
 const orm = {
     selectAll(table, callback) {
-        connection.query(`SELECT * FROM ?`, [table], (error, results) => {
+        connection.query(`SELECT * FROM ${ table }`, (error, results) => {
             if (error) throw error;
 
             callback(results);
         });
     },
-    insertOne(table, burger_name, callback) {
-        connection.query(`INSERT INTO ? (burger_name) VALUES (?)`, [table, burger_name], (error, results) => {
+    insertOne(table, col, value, callback) {
+        connection.query(`INSERT INTO ${table} (${col}) VALUES (${value})`, (error, results) => {
             if (error) throw error;
 
             callback(results);
         });
     },
-    updateOne(table, burger_name, callback) {
-        connection.query(`INSERT INTO ? (burger_name) VALUES (?)`, [table, burger_name], (error, results) => {
+    updateOne(table, col, value, id, callback) {
+        connection.query(`UPDATE ${table} SET ${col} = ${value} WHERE id = ${id}`, (error, results) => {
             if (error) throw error;
 
             callback(results);
